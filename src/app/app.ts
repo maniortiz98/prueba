5f5d48cbadfbff8985d3ec8100c62000aee1925e
mani <mani@gmail.com>
2025-10-07 15:11:19 -0400

    return

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-// Extend the Window interface to include 'env'
-declare global {
-  interface Window {
-    env?: {
-      apiUrl?: string;
-      GCP_KEY?: string;
-      [key: string]: any;
-    };
-  }
-}
+import { env } from './env.config';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'mani';
 
-  // Leer env en runtime desde window
-  apiUrl = window['env']?.apiUrl || '';
-  gcpKey = window['env']?.GCP_KEY || '';
-
   constructor() {
-    console.log('API URL:', this.apiUrl);
-    console.log('GCP Key:', this.gcpKey);
+    console.log('API URL:', env.API_URL);
+    console.log('GCP Key:', env.GCP_KEY);
   }
 }
