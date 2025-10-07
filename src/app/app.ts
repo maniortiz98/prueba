3ccb199ad7e4658044f2b8032bdcee3224810491
mani <mani@gmail.com>
2025-10-07 14:23:40 -0400

    iu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { env } from './env.config';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'mani';
+
+  constructor() {
+    console.log('API URL:', env.API_URL);
+    console.log('GCP Key:', env.GCP_KEY);
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://miapi-en-produccion.com/api',
+  gcpKey: 'valor-produccion'
+};
