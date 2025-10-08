b3b4b56dac30f91660af3d6c6e0784b730737693
mani <mani@gmail.com>
2025-10-08 16:00:23 -0400

    config

--- src/app/env.config.ts
+++
@@ -1,7 +0,0 @@
-// Configuración de variables de entorno desde .env
-// Este archivo se genera automáticamente desde generate-env.js
-
-export const env = {
-  API_URL: 'https://miapi-en-produccion.com/api',
-  GCP_KEY: 'valor-produccion'
-};
